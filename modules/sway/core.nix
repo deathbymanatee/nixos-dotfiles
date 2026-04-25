1ca5395af4b4b14f9bccb8d6ce502b38bfe702e6
deathbymanatee <rboudrx4@gmail.com>
2026-04-25 15:50:40 -0500

    updates

--- modules/sway/core.nix
+++ modules/sway/core.nix
@@ -5,8 +5,31 @@
 }:
 
 {
-  services.displayManager.lemurs = {
-    enable = true;
+  services = {
+    displayManager.lemurs.enable = true;
+    gvfs.enable = true;
+    tumbler.enable = true;
+    # sudoless brightness controls udev rules
+    # THANKS FOR NOTHING NIXOS WIKI, IT DID NOT "JUST WORK"
+    udev.extraRules = ''
+      ACTION=="add", SUBSYSTEM=="backlight", RUN+="${pkgs.coreutils}/bin/chgrp video /sys/class/backlight/%k/brightness"
+      ACTION=="add", SUBSYSTEM=="backlight", RUN+="${pkgs.coreutils}/bin/chmod g+w /sys/class/backlight/%k/brightness"
+      ACTION=="add", SUBSYSTEM=="leds", RUN+="${pkgs.coreutils}/bin/chgrp input /sys/class/leds/%k/brightness"
+      ACTION=="add", SUBSYSTEM=="leds", RUN+="${pkgs.coreutils}/bin/chmod g+w /sys/class/leds/%k/brightness"
+    '';
+    auto-cpufreq = {
+      enable = true;
+      settings = {
+        battery = {
+          governor = "powersave";
+          turbo = "never";
+        };
+        charger = {
+          governor = "performance";
+          turbo = "auto";
+        };
+      };
+    };
   };
 
   environment = {
@@ -21,9 +44,6 @@
       export XDG_DATA_DIRS=“$XDG_DATA_DIRS:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}”
     '';
   };
-
-  services.gvfs.enable = true; # Mount, trash, and other functionalities
-  services.tumbler.enable = true; # Thumbnail support for images
 
   programs = {
     sway = {
@@ -61,44 +81,41 @@
     };
   };
 
-  security.pam.services.swaylock = { };
+  security = {
+    pam = {
+      services.swaylock = { };
+      loginLimits = [
+        {
 
-  security.pam.loginLimits = [
-    {
-      domain = "@users";
-      item = "rtprio";
-      type = "-";
-      value = 1;
-    }
-  ];
+          domain = "@users";
+          item = "rtprio";
+          type = "-";
+          value = 1;
+        }
+      ];
+    };
+    polkit = {
+      enable = true;
+      extraConfig = ''
+        polkit.addRule(function (action, subject) {
+          if (
+            subject.isInGroup("power") &&
+            [
+              "org.freedesktop.login1.reboot",
+              "org.freedesktop.login1.reboot-multiple-sessions",
+              "org.freedesktop.login1.power-off",
+              "org.freedesktop.login1.power-off-multiple-sessions",
+            ].indexOf(action.id) !== -1
+          ) {
+            return polkit.Result.YES;
+          }
+        });
+      '';
 
-  security.polkit.enable = true;
-  security.soteria.enable = true;
+    };
+    soteria.enable = true;
+  };
 
-  # sudoless power commands
+  # part sudoless power commands config
   users.groups.power = { };
-  security.polkit.extraConfig = ''
-    polkit.addRule(function (action, subject) {
-      if (
-        subject.isInGroup("power") &&
-        [
-          "org.freedesktop.login1.reboot",
-          "org.freedesktop.login1.reboot-multiple-sessions",
-          "org.freedesktop.login1.power-off",
-          "org.freedesktop.login1.power-off-multiple-sessions",
-        ].indexOf(action.id) !== -1
-      ) {
-        return polkit.Result.YES;
-      }
-    });
-  '';
-
-  # sudoless brightness controls udev rules
-  # THANKS FOR NOTHING NIXOS WIKI, IT DID NOT "JUST WORK"
-  services.udev.extraRules = ''
-    ACTION=="add", SUBSYSTEM=="backlight", RUN+="${pkgs.coreutils}/bin/chgrp video /sys/class/backlight/%k/brightness"
-    ACTION=="add", SUBSYSTEM=="backlight", RUN+="${pkgs.coreutils}/bin/chmod g+w /sys/class/backlight/%k/brightness"
-    ACTION=="add", SUBSYSTEM=="leds", RUN+="${pkgs.coreutils}/bin/chgrp input /sys/class/leds/%k/brightness"
-    ACTION=="add", SUBSYSTEM=="leds", RUN+="${pkgs.coreutils}/bin/chmod g+w /sys/class/leds/%k/brightness"
-  '';
 }
